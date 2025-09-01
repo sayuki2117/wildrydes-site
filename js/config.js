2cb8d18eb7b2724a12b585e23a9d0ac6c082bbef
sayuki2117 <105279442+sayuki2117@users.noreply.github.com>
2025-09-01 09:57:26 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,10 +2,11 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_iFccrvZqr', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '6baf5nru8oetvjp8o1j4ctefhs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
